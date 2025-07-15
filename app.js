cf96e682255b654cf044aa2286c83583890f803f
abcice <mohamedoaderu@gmail.com>
2025-07-15 10:31:28 +0300

    implemented the lifeline function and disable a button function and coded the skip button mechanic

--- app.js
+++ app.js
@@ -1,71 +1,6 @@
-// START GAME
-
-// DISPLAY welcome screen
-//   WAIT for player to click “Start Game”
-
-// ASK player to enter name
-//   STORE player name
-// The game will have 2 categories
-
-// DISPLAY category choices: [Scientific] [General]
-//   WAIT for player to choose a category
-//   LOAD relevant question set
-
-// SET score to 0
-// each question will give 10 points
-//The player will wine once he achieve 100 point
-//if the player couldn't answer 3 questions  he will lose
-// SET timer to 60 seconds per question
 // SET lifelines (removeTwo, hint, skip) to 3 uses total
-
-// FOR each question in the selected category:
-//   DISPLAY question + 4 answer choices
-//there will be 15 questions for each category
-
-//   START 60-second countdown timer
-//     IF time < 10s, FLASH red warning
-
 //   WAIT for:
-//     - Player selects an answer → CHECK if correct → UPDATE score
 //     - Player uses lifeline → APPLY effect
-//     - Timer runs out → TREAT as wrong answer
-
-//   DISPLAY result (correct or wrong)
-//the correct answer will be highlighted in green 
-//if the wrong answer was choosen it will be highlighted in red and the correct answer will be shown in green
-// the hover color will be removed and the button will be unclickable  
-// GO to next question or END if out of questions
-
-// IF player answered all questions:
-//   DISPLAY congratulations message with name + final score
-//the player will lose score with each wrong choice
-//a rest button to play again if the player have either won or lose
-
-// ELSE IF player lost (e.g. 3 wrong answers or timer runs out too often):
-//   DISPLAY game over message + image
-
-// DISPLAY "Play Again" button → CALL init() to reset game
-
-// the functions that may be use:
-// event listeners
-// Arrays
-// objects
-// variable
-// functions
-// if statements
-// dome manipulations
-// the questions will change randomly by generating a random number to choose the index of the array 
-// Welcome screen
-// an input to register the name
-// 2 categories to choose from (scientific and general)
-// question screen it will have a question and 4 options
-// 3 lifelines to choose from (hint, remove 2 options, skiq questions)
-// timer
-// score counter
-
-// you won screen
-// you lose screen with try again button
-
 
 // ----- Objects------//
 const sciQ = [
@@ -263,6 +198,9 @@ let alreadyAnswered = false;
 let wrongQ = 0;
 let remainingTime = 60;
 let runningTime;
+let skipUsed = false;
+let hintUsed = false;
+let rmvUsed = false;
 
 // ------constants------//
         //-pages-//
@@ -270,7 +208,8 @@ const welcomePg = document.querySelector('#welcome-page');
 const category = document.querySelector('#Category');
 const qtnPg = document.querySelector('#Question-pg');
 const resultPg = document.querySelector('#ResultPg')
-const resetScreen = document.querySelector('#resetmsg')
+const resetScreen = document.querySelector('#resetmsg');
+const lifeScreen = document.querySelector('#lifelns')
         //-inputs-//
 const nameInput = document.querySelector('#player-name');
         //-text-//
@@ -279,7 +218,7 @@ const qtnNo = document.querySelector('#Qno');
 const resultMsg = document.querySelector('#ResultMsg');
         //-information-//
 const info = document.querySelector('#infoBox');
-const timer = document.querySelector('#Timer')
+const timer = document.querySelector('#Timer');
         //-buttons-//
 const saveBtn = document.querySelector('#save-btn');
 const startBtn = document.querySelector('#start-btn');
@@ -290,6 +229,11 @@ const opB = document.querySelector('#opB');
 const opC = document.querySelector('#opC');
 const opD = document.querySelector('#opD');
 const nextBtn = document.querySelector('#nextbtn');
+const lifeLines = document.querySelector('#Life');
+const xExit = document.querySelector('.close');
+const hnt = document.querySelector('#hint');
+const rmv = document.querySelector('#Rmv');
+const skip = document.querySelector('#skip');
 const resetBtn = document.querySelector('#Reset');
 const yesReset = document.querySelector('#yes');
 const noReset = document.querySelector('#no');
@@ -314,6 +258,16 @@ const newGame = () => {
     btn.disabled = false;
     btn.classList.remove('answered', 'correct', 'wrong');
   });
+  hintUsed = false;
+  rmvUsed = false;
+  skipUsed = false;
+  hnt.disabled = false;
+  rmv.disabled = false;
+  skip.disabled = false;
+  hnt.style.opacity = 1;
+  rmv.style.opacity = 1;
+  skip.style.opacity = 1;
+
   updateInfo();
 };
 const timeCounter = () => {
@@ -359,6 +313,9 @@ const timeOut = () => {
   wrongQ++;
   alreadyAnswered = true;
   gameStatus();
+  lifeLines.disabled = true;
+  lifeLines.style.opacity = 0.5;
+  lifeLines.style.cursor = "default";
 }
 
 const loadQuestion = () => {
@@ -434,7 +391,15 @@ const checkAnswer = (selectedOption) => {
     alreadyAnswered = true;
     clearInterval(runningTime);
     gameStatus();
+    lifeLines.disabled = true;
+    lifeLines.style.opacity = 0.5;
+    lifeLines.style.cursor = "default";
 
+}
+const disableBtn = (btn) => {
+btn.disabled = true;
+btn.style.opacity = 0.5;
+btn.style.cursor = "default";
 }
 
 //-------Event Listeners----//
@@ -498,6 +463,9 @@ nextBtn.addEventListener('click', () => {
     });
 
     alreadyAnswered = false; 
+    lifeLines.disabled = false;
+    lifeLines.style.opacity = 1;
+    lifeLines.style.cursor = "pointer";
 });
 resetBtn.addEventListener('click', () => {
 resetScreen.style.display = 'flex';
@@ -508,6 +476,25 @@ yesReset.addEventListener('click', () => {
 noReset.addEventListener('click', () =>{
   resetScreen.style.display = 'none';
 });
+lifeLines.addEventListener('click', () => {
+  if (lifeLines.disabled) return;
+  lifeScreen.style.display = 'flex';
+
+});
+xExit.addEventListener('click', () => {
+  lifeScreen.style.display = 'none';
+});
+skip.addEventListener('click', () => {
+  if(alreadyAnswered || skipUsed) return;
+  qIndex++ ;
+  if(gameStatus()) return;
+  loadQuestion();
+  alreadyAnswered = false;
+  skipUsed = true;
+  disableBtn(skip);
+  lifeScreen.style.display = 'none';
+
+})
 
 //---Result page---//
 playAgain.addEventListener('click', () => {
